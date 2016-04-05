87326dce3ed09b38b19c18b35f709ae62afa0a6b
rjwarg <rjwarg@gmail.com>
2016-04-05 12:10:02 -0700

    initial commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by richardwarg on 4/4/2016.
+ * step 20 of learn code tutorials
+ * git started 0n 5/5/16
+ */
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl',['$scope','$http', function ($scope,$http){
+    console.log("Hello from the controller");
+    $http.get('/contactlist').success(function(response){
+        console.log("I got the data I requested");
+        $scope.contactlist = response;
+    })
+
+   }]);
