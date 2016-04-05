45a26b451eb41ebca1a310a4aadb15f1bc014a2d
rjwarg <rjwarg@gmail.com>
2016-04-05 12:22:06 -0700

    Before the mongo db get request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 /*app.get('/', function(req,res){
  res.send('Hello world from server.js');
