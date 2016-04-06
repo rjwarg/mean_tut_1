4f82c05de05a1d62de16a3ad20ba3dccbc264be2
rjwarg <rjwarg@gmail.com>
2016-04-06 14:36:02 -0700

    after the day if work, but still not working

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,15 +6,44 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http', function ($scope,$http){
     console.log("Hello from the controller");
-    $http.get('/contactlist').success(function(response){
-        console.log("I got the data I requested");
-        $scope.contactlist = response;
-    });
+    var refresh = function() {
+        $http.get('/contactlist').success(function (response) {
+            console.log("I got the data I requested");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    };
+
+    refresh();
     $scope.addContact = function(){
         console.log($scope.contact);
         $http.post('/contactlist', $scope.contact).success(function(response){
             console.log(response);
+            refresh();
         });
     };
+
+    $scope.remove = function(id){
+        console.log("remove id = " + id);
+        $http.delete('/contactlist/' +id).success(function(response){
+            console.log(response);
+            refresh();
+        })
+    };
+
+    $scope.edit = function(id) {
+        console.log('edit ' + id);
+        $http.get('/contactlist/' + id).success(function (response) {
+            console.log(response);
+            $scope.contact = response;
+        });
+    };
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' +$scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+        })
+    }
 
    }]);
